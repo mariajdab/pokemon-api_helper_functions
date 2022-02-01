1bf6a9717f2c634779d33b43b69642a00a457279
Maria Jose <mariajdab@gmail.com>
2022-02-01 17:19:31 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import requests
+import re
+
+BASE_URL = "https://pokeapi.co/api/v2/"
+
+
+def new_request(pattern):
+    response = requests.get(BASE_URL + pattern)
+    body = response.json()
+    return body
+
+
+def pokemon_name_macht():
+    # Get the number of pokemon (count)
+    query_parameter = "pokemon?limit=1"
+    body = new_request(query_parameter)
+    count = body['count']
+
+    # Get all the pokemon
+    query_parameter = "pokemon?limit={}".format(count)
+    body = new_request(query_parameter)
+
+    count_match = 0
+
+    for pokemon in body['results']:
+        name = pokemon['name']
+        if name.count('a') == 2 and 'at' in name:
+            count_match += 1
+
+    return count_match
+
+
+def species_raichu_can_procreate():
+    pattern = "pokemon-species/raichu"
+    body = new_request(pattern)
+    egg_groups = body['egg_groups']
+
+    total_species = 0
+    for egg_group in egg_groups:
+        response = requests.get(egg_group['url'])
+        body = response.json()
+        total_species += len(body['pokemon_species'])
+
+    return total_species
+
+
+def type_fighting_generation_i_pokemon():
+    pokemons = []
+    id_max_generation_1 = 151
+    fighting_type = new_request("/type/2")
+
+    for pokemon in fighting_type["pokemon"]:
+        id = re.findall('[0-9]+', pokemon["pokemon"]["url"][33:])
+
+        if int(id[0]) <= id_max_generation_1:
+            name = pokemon["pokemon"]["name"]
+            url_pattern = "pokemon/{}".format(name)
+            details = new_request(url_pattern)
+            pokemons.append(int(details["weight"]))
+
+    pokemons.sort()
+    return pokemons[-1], pokemons[0]
